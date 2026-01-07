154bd1d80cd201c45b0f6b327a6aced4002c8153
Dmytro <dimusgamer2.0@gmail.com>
2026-01-07 10:33:51 +0200

    implement tests

--- src/isIsogram.test.js
+++ src/isIsogram.test.js
@@ -6,4 +6,28 @@ describe('isIsogram', () => {
   it(`should be declared`, () => {
     expect(isIsogram).toBeInstanceOf(Function);
   });
+
+  it(`should return true for an empty string`, () => {
+    expect(isIsogram('')).toBeTruthy();
+  });
+
+  it(`should return true for 'playgrounds'`, () => {
+    expect(isIsogram('playgrounds')).toBeTruthy();
+  });
+
+  it(`should return false for 'look'`, () => {
+    expect(isIsogram('look')).toBeFalsy();
+  });
+
+  it(`should return false for 'Adam'`, () => {
+    expect(isIsogram('Adam')).toBeFalsy();
+  });
+
+  it(`should return false for 'Oops'`, () => {
+    expect(isIsogram('Oops')).toBeFalsy();
+  });
+
+  it(`should return true for 'isogram'`, () => {
+    expect(isIsogram('isogram')).toBeTruthy();
+  });
 });
